edf9d0e42a3f2f42ebb72b063b06ddf076b99225
colinpmillar <colin.millar@jrc.ec.europa.eu>
2013-10-13 22:03:50 +0200

    colins first commit

---
+++ Rscripts/a4aBET_cm.R
@@ -0,0 +1,74 @@
+# a4aBET.R - DESC
+# a4aBET.R
+
+# Copyright 2003-2013 FLR Team. Distributed under the GPL 2 or later
+# Maintainer: Iago Mosqueira, JRC
+# Soundtrack:
+# Notes:
+
+# This is what I managed to get started with today
+
+# - Changes in srmodel (geomean vs. beholt, CV value) appears to have a large effect
+
+# - I am not clear what I am doing with regards to selectivity of CPUE. It is a LL, so
+# should fully select ages 4-9, or maybe peak at 4-5 and then lower. I would like to
+# test those two alternatives.
+
+# - As you can see the CPUE is crazy, so no wonder any SA struggles
+
+library(FLa4a)
+library(ggplotFL)
+
+# BET FLStock and JAP LL FLIndex
+load('data/bet_iotc_2011.RData')
+
+# CHANGE Fully selected ages
+range(bet)[c("minfbar","maxfbar")] <- c(4,9)
+units(harvest(bet)) <- "f"
+
+# CREATE FLindices using JAPLL
+idx <- FLIndices(japll=japll)
+
+
+fmodel = ~ s(age, k = 3)
+qmodel = list( ~1)
+srmodel = ~ factor(year)
+stock = bet
+indices = idx
+covar = NULL
+wkdir = NULL
+verbose = FALSE
+fit = "assessment"
+
+
+
+# run1 - Constant q
+run1 <- a4a(fmodel = ~ s(age, k = 3) + s(year, k = 10), qmodel = list( ~1), stock = bet,
+	indices = idx)
+
+c(log(index(run1)[[1]] / index(idx[[1]])))
+plot((bet + run1))
+
+# run2 - q by year
+run2 <- a4a(fmodel = ~ s(age, k = 3) + s(year, k = 10), qmodel = list(~factor(year)), stock = bet, indices = idx)
+
+c(log(index(run2)[[1]] / index(idx[[1]])))
+plot((bet + run2))
+
+# run3 - geomean SR
+run3 <- a4a(fmodel = ~ s(age, k = 3) + s(year, k = 10), qmodel = list(~factor(year)), stock = bet, indices = idx, srmodel = ~ geomean(CV=5))
+
+log(index(run3)[[1]] / index(idx[[1]]))
+plot((bet + run3))
+
+# run4 - 
+run4 <- a4a(fmodel = ~te(age, year, k=c(4, 10)), qmodel = list(~factor(year)), stock = bet, indices = idx, srmodel = ~ bevholt(CV=0.5))
+
+log(index(run4)[[1]] / index(idx[[1]]))
+plot((bet + run4))
+
+# run5 - 
+run4 <- a4a(fmodel = ~ s(age, k=6) + factor(year), qmodel = list(japll=~factor(year)), stock = bet, indices = idx, srmodel = ~ bevholt(CV=0.5))
+
+log(index(run4)[[1]] / index(idx[[1]]))
+plot((bet + run4))
---
+++ Rscripts/simulation.R
@@ -0,0 +1,111 @@
+
+# simulation set up
+
+years <- 1990:2010
+ages <- 1:10
+
+nages <- length(ages)
+nyears <- length(years)
+
+# model data
+data <- expand.grid(age = ages, year = years)
+
+# model formulas
+fmodel <- ~ 1
+qmodel <- list(~ 1)
+#qmodel <- list(~ factor(as.numeric(age > 2)))
+srmodel <- ~ 1
+n1model <- ~ 1
+
+# design matrices
+opts <- options(contrasts = c(unordered = "contr.sum", ordered = "contr.poly")) 
+Xf <- model.matrix(fmodel, data)
+Xq <- model.matrix(qmodel[[1]], data)
+Xr <- model.matrix(srmodel, subset(data, age == ages[1]))
+Xn1 <- model.matrix(n1model, subset(data, age > ages[1] & year == years[1]))
+options(opts)
+
+# model parameters
+bf <- log(0.4)
+bq <- log(0.001)
+#bq <- log(c(0.0000001, 0.001))
+br <- log(10000)
+bn1 <- log(1000)
+lsigc <- log(0.1)
+lsigi <- log(0.1)
+
+# m, wts and maturity
+M <- matrix(0.2, nages, nyears)
+Wt <- matrix(1, nages, nyears)
+Mat <- matrix(rep(0:1, c(2, nages-2)), nages, nyears)
+pz <- 0.5
+
+## model predictions
+Fm <- matrix(exp(Xf %*% bf), nages, nyears)
+Qm <- matrix(exp(Xq %*% bq), nages, nyears)
+
+# build n at age
+n <- matrix(NA, nages, nyears)
+n[1,] <- c(Xr %*% br)
+n[-1,1] <- c(Xn1 %*% bn1)
+Z <- Fm + M
+for (i in 2:nages) {
+  n[i,-1] <- n[i-1, -nyears] - Z[i-1, -nyears]
+}
+# do plus group
+n[nages,-1] <- log( exp(n[nages,-1]) + exp(n[nages,-nyears] - Z[i-1, -nyears]) )
+
+# construct catches and indices
+catch <- log(Fm) - log(Z) + log(1 - exp(-Z)) + n
+index <- log( colSums(Wt * Qm * exp(n) * exp(-Z*pz)) )
+
+## make observations
+catchobs <- catch + rnorm(length(catch), 0, exp(lsigc))
+indexobs <- index + rnorm(length(index), 0, exp(lsigi))
+
+## build FLR objects
+require(FLCore)
+stock <- FLStock(
+           catch.n      = FLQuant(exp(catchobs), dimnames=list(age=ages, year=years)),
+           catch.wt     = FLQuant(Wt, dimnames=list(age=ages, year=years)),
+           mat          = FLQuant(Mat, dimnames=list(age=ages, year=years)),
+           m            = FLQuant(M, dimnames=list(age=ages, year=years)),
+           harvest      = FLQuant(NA, dimnames=list(age=ages, year=years), units = "f"),
+           harvest.spwn = FLQuant(0.6, dimnames=list(age=ages, year=years)),
+           m.spwn       = FLQuant(0.6, dimnames=list(age=ages, year=years)),
+           stock.wt     = FLQuant(Wt, dimnames=list(age=ages, year=years))
+         )
+catch(stock) <- computeCatch(stock)
+range(stock)[c("minfbar","maxfbar")] <- c(4,9)
+
+indices <- FLIndices(simsurv = 
+             FLIndex(
+               index = FLQuant(exp(indexobs), dimnames=list(age="all", year=years))
+             ))
+range(indices[[1]])[c("startf","endf")] <- 0.5
+
+## fit model
+covar = NULL
+wkdir = NULL
+verbose = FALSE
+fit = "assessment"
+vmodel = list(~1, ~1)
+
+source("fitting_code_snippet.R")
+
+## assess the fit
+
+est <- out $ par.est[1:5]
+
+sim <- list(fpar = bf, qpar = bq, vpar = c(lsigc, lsigi), ny1par = bn1, rpar = br)
+
+summ <- cbind.data.frame(sim = unlist(sim), est = unlist(est))
+summ $ diff <- with(summ, (est - sim))
+summ $ sdiff <- with(summ, abs(diff / sim) )
+
+print(summ)
+
+
+
+
+
